af9b5405d3341030538194982ccf96df7ac65fcb
nihadelkhattabi2 <152630050+nihadelkhattabi2@users.noreply.github.com>
2024-04-28 03:20:13 +0100

    singup in firebase

---
+++ lib/Application etudiant/auth/signup.dart
@@ -0,0 +1,138 @@
+// ignore_for_file: prefer_const_constructors, non_constant_identifier_names
+
+import 'package:application_gestion_des_reclamations_pfe/Application%20etudiant/auth/login.dart';
+import 'package:application_gestion_des_reclamations_pfe/Application%20etudiant/components/custombuttonauth.dart';
+import 'package:application_gestion_des_reclamations_pfe/Application%20etudiant/components/customlogoauth.dart';
+import 'package:application_gestion_des_reclamations_pfe/Application%20etudiant/components/textformfield.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  TextEditingController nom = TextEditingController();
+  TextEditingController Prenom = TextEditingController();
+  TextEditingController Appoge = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: ListView(children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(height: 50),
+              const CustomLogoAuth(),
+              Container(height: 20),
+              const Text("SignUp",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              Container(height: 10),
+              const Text("SignUp To Continue Using The App",
+                  style: TextStyle(color: Colors.grey)),
+              Container(height: 20),
+              const Text(
+                "Nom ",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Container(height: 10),
+              CustomTextForm(hinttext: "ُEnter votre nom", mycontroller: nom),
+              Container(height: 20),
+              const Text(
+                "Prenom ",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Container(height: 10),
+              CustomTextForm(
+                  hinttext: "ُEnter votre prenom", mycontroller: Prenom),
+              Container(height: 20),
+              const Text(
+                "Appoge ",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Container(height: 10),
+              CustomTextForm(
+                  hinttext: "ُEnter votre Appoge", mycontroller: Appoge),
+              Container(height: 20),
+              const Text(
+                "Email",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Container(height: 10),
+              CustomTextForm(
+                  hinttext: "ُEnter Your Email", mycontroller: email),
+              Container(height: 10),
+              const Text(
+                "Password",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Container(height: 10),
+              CustomTextForm(
+                  hinttext: "ُEnter Your Password", mycontroller: password),
+              Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 20),
+                alignment: Alignment.topRight,
+                child: const Text(
+                  "Forgot Password ?",
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          CustomButtonAuth(
+              title: "SignUp",
+              onPressed: () async {
+                try {
+                  // ignore: unused_local_variable
+                  final credential = await FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                    email: email.text,
+                    password: password.text,
+                  );
+                  Navigator.of(context).pushReplacementNamed("Login");
+
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    print('The password provided is too weak.');
+                  } else if (e.code == 'email-already-in-use') {
+                    print('The account already exists for that email.');
+                  }
+                } catch (e) {
+                  print(e);
+                }
+              }),
+          Container(height: 20),
+          // Text("Don't Have An Account ? Resister" , textAlign: TextAlign.center,)
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Login()));
+            },
+            child: const Center(
+              child: Text.rich(TextSpan(children: [
+                TextSpan(
+                  text: "Have An Account ? ",
+                ),
+                TextSpan(
+                    text: "Login",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 43, 119, 211),
+                        fontWeight: FontWeight.bold)),
+              ])),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,37 @@
-// ignore_for_file: unused_import
+/// ignore_for_file: unused_import
 
-import 'package:application_gestion_des_reclamations_pfe/Application%20enseignant/sign_in_enseignant.dart';
-import 'package:application_gestion_des_reclamations_pfe/Application%20etudiant/AjouterReclamation.dart';
-import 'package:application_gestion_des_reclamations_pfe/Application%20etudiant/Login.dart';
-import 'package:application_gestion_des_reclamations_pfe/Application%20etudiant/Signup.dart';
-import 'package:flutter/material.dart';
+
+import 'package:application_gestion_des_reclamations_pfe/Application%20etudiant/auth/login.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Correct import
 import 'package:firebase_core/firebase_core.dart';
-
-
-import 'firebase_options.dart';
-
+import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+  await Firebase.initializeApp();
+  
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-
-          // primaryColor:  Color.fromARGB(95, 20, 80, 243),
-
-          ),
+      theme: ThemeData(),
       debugShowCheckedModeBanner: false,
-
-
-      home:LoginPage(),
-
- 
-      
-
+      home: Login(),
     );
   }
+}
+
+void initState() {
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user == null) {
+      print('=================================User is currently signed out!');
+    } else {
+      print('==================================User is signed in!');
+    }
+  });
 }
